acf034658fdd2d4db01087cf7f6056eb5159a048
Andrew Gerst <gerst20051@gmail.com>
2015-01-25 23:18:10 -0500

    added notes for piezo buzzer

---
+++ Sketches/Piezo Buzzer/notes.h
@@ -0,0 +1,149 @@
+/*************************************************
+ * Tone Frequencies
+ * http://api.ning.com/files/BVQDZoqdx*9pEUENbk4DVmcrURfee-ie3wPioV*qNQ0YWjaF0ffXzBeB5QB8ckGjBwFOFul0jeoRnyBPohiJWbhvze11FIqH/NOTAS.png
+ *************************************************/
+
+#define NO_SOUND 0
+
+#define NOTE_C0  16 // -48
+#define NOTE_CS0 17 // -47
+#define NOTE_D0  18 // -46
+#define NOTE_DS0 19 // -45
+#define NOTE_E0  21 // -44
+#define NOTE_F0  22 // -43
+#define NOTE_FS0 23 // -42
+#define NOTE_G0  25 // -41
+#define NOTE_GS0 26 // -40
+#define NOTE_A0  28 // -39
+#define NOTE_AS0 29 // -38
+#define NOTE_B0  31 // -37
+
+#define NOTE_C1  33 // -36
+#define NOTE_CS1 35 // -35
+#define NOTE_D1  37 // -34
+#define NOTE_DS1 39 // -33
+#define NOTE_E1  41 // -32
+#define NOTE_F1  44 // -31
+#define NOTE_FS1 46 // -30
+#define NOTE_G1  49 // -29
+#define NOTE_GS1 52 // -28
+#define NOTE_A1  55 // -27
+#define NOTE_AS1 58 // -26
+#define NOTE_B1  62 // -25
+
+#define NOTE_C2  65 // -24
+#define NOTE_CS2 69 // -23
+#define NOTE_D2  73 // -22
+#define NOTE_DS2 78 // -21
+#define NOTE_E2  82 // -20
+#define NOTE_F2  87 // -19
+#define NOTE_FS2 93 // -18
+#define NOTE_G2  98 // -17
+#define NOTE_GS2 104 // -16
+#define NOTE_A2  110 // -15
+#define NOTE_AS2 117 // -14
+#define NOTE_B2  123 // -13
+
+#define NOTE_C3  131 // -12
+#define NOTE_CS3 139 // -11
+#define NOTE_D3  147 // -10
+#define NOTE_DS3 156 // -9
+#define NOTE_E3  165 // -8
+#define NOTE_F3  175 // -7
+#define NOTE_FS3 185 // -6
+#define NOTE_G3  196 // -5
+#define NOTE_GS3 208 // -4
+#define NOTE_A3  220 // -3
+#define NOTE_AS3 233 // -2
+#define NOTE_B3  247 // -1
+
+#define NOTE_C4  262 // Middle C
+#define NOTE_CS4 277 // +1
+#define NOTE_D4  294 // +2
+#define NOTE_DS4 311 // +3
+#define NOTE_E4  330 // +4
+#define NOTE_F4  349 // +5
+#define NOTE_FS4 370 // +6
+#define NOTE_G4  392 // +7
+#define NOTE_GS4 415 // +8
+#define NOTE_A4  440 // +9
+#define NOTE_AS4 466 // +10
+#define NOTE_B4  494 // +11
+
+#define NOTE_C5  523 // +12
+#define NOTE_CS5 554 // +13
+#define NOTE_D5  587 // +14
+#define NOTE_DS5 622 // +15
+#define NOTE_E5  659 // +16
+#define NOTE_F5  698 // +17
+#define NOTE_FS5 740 // +18
+#define NOTE_G5  784 // +19
+#define NOTE_GS5 831 // +20
+#define NOTE_A5  880 // +21
+#define NOTE_AS5 932 // +22
+#define NOTE_B5  988 // +23
+
+#define NOTE_C6  1047 // +24
+#define NOTE_CS6 1109 // +25
+#define NOTE_D6  1175 // +26
+#define NOTE_DS6 1245 // +27
+#define NOTE_E6  1319 // +28
+#define NOTE_F6  1397 // +29
+#define NOTE_FS6 1480 // +30
+#define NOTE_G6  1568 // +31
+#define NOTE_GS6 1661 // +32
+#define NOTE_A6  1760 // +33
+#define NOTE_AS6 1865 // +34
+#define NOTE_B6  1976 // +35
+
+#define NOTE_C7  2093 // +36
+#define NOTE_CS7 2218 // +37
+#define NOTE_D7  2349 // +38
+#define NOTE_DS7 2489 // +39
+#define NOTE_E7  2637 // +40
+#define NOTE_F7  2794 // +41
+#define NOTE_FS7 2960 // +42
+#define NOTE_G7  3136 // +43
+#define NOTE_GS7 3322 // +44
+#define NOTE_A7  3520 // +45
+#define NOTE_AS7 3729 // +46
+#define NOTE_B7  3951 // +47
+
+#define NOTE_C8  4186 // +48
+#define NOTE_CS8 4435 // +49
+#define NOTE_D8  4699 // +50
+#define NOTE_DS8 4978 // +51
+#define NOTE_E8  5274 // +52
+#define NOTE_F8  5588 // +53
+#define NOTE_FS8 5920 // +54
+#define NOTE_G8  6272 // +55
+#define NOTE_GS8 6645 // +56
+#define NOTE_A8  7040 // +57
+#define NOTE_AS8 7459 // +58
+#define NOTE_B8  7902 // +59
+
+#define NOTE_C9  8372 // +60
+#define NOTE_CS9 8870 // +61
+#define NOTE_D9  9397 // +62
+#define NOTE_DS9 9956 // +63
+#define NOTE_E9  10548 // +64
+#define NOTE_F9  11175 // +65
+#define NOTE_FS9 11840 // +66
+#define NOTE_G9  12544 // +67
+#define NOTE_GS9 13290 // +68
+#define NOTE_A9  14080 // +69
+#define NOTE_AS9 14917 // +70
+#define NOTE_B9  15804 // +71
+
+#define NOTE_C10  16744 // +72
+#define NOTE_CS10 17740 // +73
+#define NOTE_D10  18795 // +74
+#define NOTE_DS10 19912 // +75
+#define NOTE_E10  21096 // +76
+#define NOTE_F10  22350 // +77
+#define NOTE_FS10 23680 // +78
+#define NOTE_G10  25088 // +79
+#define NOTE_GS10 26580 // +80
+#define NOTE_A10  28160 // +81
+#define NOTE_AS10 29835 // +82
+#define NOTE_B10  31609 // +83
